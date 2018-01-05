821a7aba2c725fd7cdbfa2712d1b18f636bacb7c
tannt275 <tannt275@gmail.com>
2018-01-05 22:35:51 +0700

    modified master for first use

--- app/src/main/java/kampus/vn/MainActivity.java
+++ app/src/main/java/kampus/vn/MainActivity.java
@@ -2,6 +2,10 @@ package kampus.vn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> objects = new ArrayList<>();
+        objects.add("Ha Dong");
+        objects.add("Hoa Binh");
+        objects.add("Nghe An");
+        objects.add("Hai Duong");
+        objects.add("Ha Tinh");
+        objects.add("Nha Trang");
+        objects.add("Bac Giang");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, objects);
+        listView.setAdapter(adapter);
     }
 }
